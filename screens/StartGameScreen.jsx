5ad30a97b5b1c24dec6c14e4faaeee0347504a9b
amoore3234 <akmoore@cpp.edu>
2022-08-26 14:32:18 -0700

    Styling StartGameScreen component
    -Add custom pressable button.
    -Applied styles to button to improve UI.
    -Changed text input into number input.
    -Add a gradient backround.
    -Add conditions to validate number inputs.

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,22 +1,65 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
+
 import { PrimaryButton } from "../components/PrimaryButton";
 
 export const StartGameScreen = () => {
+
+	const [enteredNumber, setEnteredNumber] = useState('');
+
+	const numberInputHandler = (enteredText) => {
+		setEnteredNumber(enteredText);
+	}
+
+	const resetInputHandler = () => {
+		setEnteredNumber('');
+	}
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredNumber);
+
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert(
+				'Invalid number',
+				'Number has to be a number between 1 and 99',
+				[{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+			);
+			return;
+		}
+		console.log('Valid number');
+	}
+
   return (
     <View style={styles.inputContainer}>
-			<TextInput style={styles.numberInput} maxLength={2} />
-			<PrimaryButton>Reset</PrimaryButton>
-			<PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+				autoCorrect={false}
+				onChangeText={numberInputHandler}
+				value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
 	inputContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
 		marginTop: 100,
 		marginHorizontal: 24,
 		padding: 16,
-		backgroundColor: '#72063c',
+		backgroundColor: '#3b021f',
 		borderRadius: 8,
 		elevation: 4,
 		shadowColor: 'black',
@@ -34,5 +77,11 @@ const styles = StyleSheet.create({
 		marginVertical: 8,
 		fontWeight: 'bold',
 		textAlign: 'center'
+	},
+	buttonsContainer: {
+		flexDirection: 'row'
+	},
+	buttonContainer: {
+		flex: 1
 	}
 });
