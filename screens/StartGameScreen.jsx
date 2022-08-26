4ec9abda593e7d2bd93b385fdb0b98de01f5cd45
amoore3234 <akmoore@cpp.edu>
2022-08-26 16:34:07 -0700

    Style GameScreen component
    -Add functionality to switch from the StartGameScreen to the GameScreen.
    -Add Title as custom style component.
    -Add Color util component to store hex color codes.
    -Add functionality for computer to guess number.
    -Implement dummy text in GameOverScreen component.
    -Add SplashScreen functionality and imported custom fonts.

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -60,22 +60,27 @@ export const StartGameScreen = ({ onPickNumber }) => {
 };
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    marginTop: 100,
+  inputContainer: {
+    justifyContent: "center",
     alignItems: "center",
-  },
-  instructionText: {
-    color: Colors.accent500,
-    fontSize: 24,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#3b021f",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
   numberInput: {
     height: 50,
     width: 50,
-    fontSize: 24,
-    borderBottomColor: Colors.accent500,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
-    color: Colors.accent500,
+    color: "#ddb52f",
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
